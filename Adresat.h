bbeba8940fc3b9c396112883dfc7a830fb3068c1
Artur Petrzak <petrzmax@gmail.com>
2020-10-13 23:06:07 +0200

    Update Adresat class

--- Adresat.h
+++ Adresat.h
@@ -7,11 +7,12 @@ using namespace std;
 
 class Adresat
 {
-    int id;
+    int id, idUzytkownika;
     string imie, nazwisko, numerTelefonu, email, adres;
 
 public:
     void ustawId(int noweId);
+    void ustawIdUzytkownika(int noweIdUzytkownika);
     void ustawImie(string noweImie);
     void ustawNazwisko (string noweNazwisko);
     void ustawNumerTelefonu(string nowyNumerTelefonu);
@@ -19,6 +20,7 @@ public:
     void ustawAdres(string nowyAdres);
 
     int pobierzId();
+    int pobierzIdUzytkownika();
     string pobierzImie();
     string pobierzNazwisko();
     string pobierzNumerTelefonu();
