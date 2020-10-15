0b4ed5c738b8e2d2fb19ddbd38dbc125b0797240
Artur Petrzak <petrzmax@gmail.com>
2020-10-15 17:11:21 +0200

    Added necessary methods to MetodyPomocnicze

--- MetodyPomocnicze.cpp
+++ MetodyPomocnicze.cpp
@@ -15,6 +15,42 @@ string MetodyPomocnicze::wczytajLinie()
     return wejscie;
 }
 
+char MetodyPomocnicze::wczytajZnak()
+{
+    string wejscie = "";
+    char znak  = {0};
+
+    while (true)
+    {
+        getline(cin, wejscie);
+
+        if (wejscie.length() == 1)
+        {
+            znak = wejscie[0];
+            break;
+        }
+        cout << "To nie jest pojedynczy znak. Wpisz ponownie." << endl;
+    }
+    return znak;
+}
+
+int MetodyPomocnicze::wczytajLiczbeCalkowita()
+{
+    string wejscie = "";
+    int liczba = 0;
+
+    while (true)
+    {
+        getline(cin, wejscie);
+
+        stringstream myStream(wejscie);
+        if (myStream >> liczba)
+            break;
+        cout << "To nie jest liczba. Wpisz ponownie. " << endl;
+    }
+    return liczba;
+}
+
 bool MetodyPomocnicze::czyPlikJestPusty(fstream &plikTekstowy)
 {
     plikTekstowy.seekg(0, ios::end);
