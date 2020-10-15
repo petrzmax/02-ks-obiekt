6bc27deb32999732d623efbc40ad06f9b67e3563
Artur Petrzak <petrzmax@gmail.com>
2020-10-15 21:17:54 +0200

    Make PlikZUzytkownikami inherit from PlikTekstowy

--- PlikZUzytkownikami.h
+++ PlikZUzytkownikami.h
@@ -2,26 +2,22 @@
 #define PLIKZUZYTKOWNIKAMI_H
 
 #include <iostream>
-#include <fstream>
 #include <vector>
 #include <cstdlib>
 
+#include "PlikTekstowy.h"
 #include "Uzytkownik.h"
 #include "MetodyPomocnicze.h"
 
 using namespace std;
 
-class PlikZUzytkownikami
+class PlikZUzytkownikami : public PlikTekstowy
 {
-    const string NAZWA_PLIKU_Z_UZYTKOWNIKAMI;
-
-
     string zamienDaneUzytkownikaNaLinieZDanymiOddzielonaPionowymiKreskami(Uzytkownik uzytkownik);
     Uzytkownik pobierzDaneUzytkownika(string daneJednegoUzytkownikaOddzielonePionowymiKreskami);
 
-
 public:
-    PlikZUzytkownikami(string nazwaPlikuZUzytkownikami) : NAZWA_PLIKU_Z_UZYTKOWNIKAMI(nazwaPlikuZUzytkownikami) {};
+    PlikZUzytkownikami(string nazwaPlikuZUzytkownikami) : PlikTekstowy(nazwaPlikuZUzytkownikami) {};
     void dopiszUzytkownikaDoPliku(Uzytkownik uzytkownik);
     void zapiszWszystkichUzytkownikowDoPliku(vector <Uzytkownik> &uzytkownicy);
     vector<Uzytkownik> wczytajUzytkownikowZPliku();
